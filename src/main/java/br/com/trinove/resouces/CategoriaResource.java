ef912c7740dbe6b1a0f4acd9c311de3703b23863
Lucas Assis Lima <lucas.lima@trinove.com.br>
2022-03-07 10:30:48 -0300

    Listando todas categorias com DTO

--- src/main/java/br/com/trinove/resouces/CategoriaResource.java
+++ src/main/java/br/com/trinove/resouces/CategoriaResource.java
@@ -2,8 +2,7 @@ package br.com.trinove.resouces;
 
 import java.net.URI;
 import java.util.List;
-
-import javax.servlet.Servlet;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.trinove.domain.Categoria;
+import br.com.trinove.dto.CategoriaDTO;
 import br.com.trinove.services.CategoriaService;
 
 
@@ -54,6 +54,14 @@ public class CategoriaResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 		
+	}
+	
+	@RequestMapping( method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll(){
+		
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 	
 
